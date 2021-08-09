b14fa29e37a3faf7d95c7a8a9f5fb9ed4e7a6e9e
Manuel E. Hernandez <kicke10h@gmail.com>
2021-08-08 19:20:18 -0500

    Add the server

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,7 +1,21 @@
-import serial, time
+from fastapi import FastAPI
+from fastapi_socketio import SocketManager
+
+import serial, time, uvicorn
+
 ser = serial.Serial('/dev/cu.usbserial-1410', 9600, timeout=1)
-time.sleep(2)
-while True:
-    data = ser.readline()
-    print(data.decode('utf-8'))
+
+
+app = FastAPI()
+socket = SocketManager(app)
+
+@app.get('/')
+def root():
+    return {"Hello": "world"}
+
+@app.get('/distance')
+def distance():
+    distance = ser.readline()
+    return {"distance":str(distance.decode("utf-8"))}
+
 
